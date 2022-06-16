5899c3ff9acbe02dcd6c7f6e4488a54e8f4c116d
SlaviGrigorov <slavibg@mail.bg>
2022-06-16 16:58:14 +0300

    Create POST accessory endpoint

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -4,4 +4,10 @@ router.get('/create', (req, res) => {
     res.render('createAccessory');
 });
 
+router.post('/create', (req, res) => {
+    let accessory = req.body;
+    console.log(accessory);
+
+    // res.status(200).send(`Accessory send ${accessory.imageUrl}`);
+});
 module.exports = router;
