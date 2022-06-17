0a70db0137d1544d3a22fe4598f24cef2caf461f
SlaviGrigorov <slavibg@mail.bg>
2022-06-17 14:33:49 +0300

    Add login and register pages

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 router.use('/', homeController);
 router.use('/cube', cubeController);
 router.use('/accessory', accessoryController);
+router.use('/auth', authController);
 router.use('*', (req, res) => {
     res.render('404');
 });
